c3c99fd365706e579d80176d451217e9acaaa80a
gduck <dale.github@thefoos.net>
2014-12-21 15:30:45 +0800

    about to try some joins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    @user = User.new
+    #@users = User.all
+    #user = User.new
 
-    @languages = Language.all
-    @lang_profs = LanguageProf.all
+    # @languages = Language.all
+    @langprofs = LanguageProf.all.order(:user_id)
+    #@langprof = LanguageProf.select("DISTINCT(user_id)").merge(LanguageProf.group(:user_id, :language_id)).order(:user_id)
+
+    # get unique userIDs that have lang profs
+    userIDs = LanguageProf.group(:user_id)
+    @users = User.select(userIDs)
 
 
+
+    @prof_name = [
+      'Beginner', 'Conversational', 'Fluent', 'Native' ]
 
   def show
     
