380d72ebae7175724e6713c8e61cf5bb19b50082
gduck <dale.github@thefoos.net>
2014-12-24 12:59:52 +0800

    need to change user db to include first language - one required per user

--- db/migrate/20141217084824_create_users.rb
+++ db/migrate/20141217084824_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :first_language, null: false, default: "English"
 
       #t.integer :language_id
 
