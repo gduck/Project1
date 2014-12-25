2b51abe32984e87851d1689b934b801cdda3d39d
gduck <dale.github@thefoos.net>
2014-12-25 18:19:37 +0800

    fixed formatting to allow row value insertion for each language

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,6 @@
 class Language < ActiveRecord::Base
   has_many :language_profs
   has_many :users, through: :language_profs
-  has_many :prof_categories, through: :language_profs
+  has_many :prof_categories, through: :language_profs, through: :users
 
 end
