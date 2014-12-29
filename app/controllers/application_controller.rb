f9af7fd257d7c6c9bf852615e9087e3ee59720a3
gduck <dale.github@thefoos.net>
2014-12-29 20:48:01 +0800

    trying to get heroku to take it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  #$prof_name = ['Beginner', 'Conversational', 'Fluent', 'Native' ]
+  # //need this for now
+  $prof_name = ['Beginner', 'Conversational', 'Fluent', 'Native' ]
 
 end
