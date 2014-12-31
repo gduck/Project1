acc896bda89e5b9840b7797cea9ab7622def0cfd
gduck <dale.github@thefoos.net>
2014-12-31 08:05:01 +0800

    changing first language to be an integer, consistent with lang_profs

--- db/migrate/20141217084824_create_users.rb
+++ db/migrate/20141217084824_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :first_language, null: false, default: "English"
+      t.integer :first_language, null: false, default: 1
       t.string :address_area
 
       #t.integer :language_id
