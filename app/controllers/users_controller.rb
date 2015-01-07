eb845a1537e061ea3e37ff6a561ba22a7365b729
gduck <dale.github@thefoos.net>
2015-01-07 21:28:23 +0800

    changed the render of @users which fixed my ajax search on index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
 
   def index
     @users_with_lang_profs = User.search(params[:search])
+    @users = @users_with_lang_profs
     @languages = Language.all
   end
 
