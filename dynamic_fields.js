924e0bf591f99e4e6182c8149334c7d13b928548
gduck <dale.github@thefoos.net>
2015-01-12 10:04:17 +0800

    fixed navbar

--- dynamic_fields.js
+++ dynamic_fields.js
@@ -1,26 +1,19 @@
+  function add_fields(link, association, content) {
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("new_language_profs", "g");
+    $('#other-lang').append(content.replace(regexp, new_id));
+  };
+
 module ApplicationHelper
-  def link_to_remove_fields(name, f)
-    # for some reason this hidden field below doesnt work
-    # have put it in form template instead
-    #f.hidden_field(:_destroy), {:class => "killme"})
-    link_to name, "javascript:", :onclick => "remove_fields(this);"
-  end
 
   def link_to_add_fields(name, f, association)
     # f here must be from user_form for correct association
     new_object = f.object.class.reflect_on_association(association).klass.new
 
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      
       render(association.to_s.singularize + "_fields", :f => builder)
     end
-
     link_to name, "javascript:", :onclick => "add_fields(this, \"association\", \"#{escape_javascript(fields)}\");"
-
   end
-
-  def is_active?(link_path)
-    current_page?(link_path) ? "active" : ""
-  end
-    
+   
 end
